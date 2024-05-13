df03506201dfbf3884ce7797f63cb1964884826e
Nicolasq <quinet.nicolas@gmail.com>
2024-05-13 12:07:10 +0200

    Première étape

---
+++ src/main/java/be/java/gestiondossierjudiciare/GestionDossierJudiciareApplication.java
@@ -0,0 +1,13 @@
+package be.java.gestiondossierjudiciare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDossierJudiciareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDossierJudiciareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/java/gestiondossierjudiciare/GestionDossierJudiciareApplicationTests.java
@@ -0,0 +1,13 @@
+package be.java.gestiondossierjudiciare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDossierJudiciareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
