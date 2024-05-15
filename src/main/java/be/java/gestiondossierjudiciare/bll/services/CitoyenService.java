4b1c4ebe09d68e09561dc60202177ef3194de795
Azzedine Hassaini <azzedinehassaini@gmail.com>
2024-05-15 11:07:47 +0200

    Citoyen update.

--- src/main/java/be/java/gestiondossierjudiciare/bll/services/CitoyenService.java
+++ src/main/java/be/java/gestiondossierjudiciare/bll/services/CitoyenService.java
@@ -4,6 +4,6 @@ import be.java.gestiondossierjudiciare.domain.entities.Citoyen;
 
 public interface CitoyenService {
 
-    Long update(String id, Citoyen citoyen);
+    Long update(Long id, Citoyen citoyen);
 
 }
