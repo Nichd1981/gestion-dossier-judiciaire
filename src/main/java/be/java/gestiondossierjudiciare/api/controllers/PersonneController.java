072f238fbdc2516f6bcab0fef4db421641c2d349
Azzedine Hassaini <azzedinehassaini@gmail.com>
2024-05-16 12:51:12 +0200

    Correction CitoyenController->PersonneController

--- src/main/java/be/java/gestiondossierjudiciare/api/controllers/PersonneController.java
+++ src/main/java/be/java/gestiondossierjudiciare/api/controllers/PersonneController.java
@@ -2,9 +2,12 @@ package be.java.gestiondossierjudiciare.api.controllers;
 
 import be.java.gestiondossierjudiciare.api.forms.PersonneUpdateForm;
 import be.java.gestiondossierjudiciare.bll.services.PersonneService;
+import be.java.gestiondossierjudiciare.domain.entities.Utilisateur;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,6 +20,17 @@ public class PersonneController {
     @PutMapping("/{id:\\d+}")
     public ResponseEntity<Long> updatePersonne(@PathVariable Long id, @RequestBody @Valid PersonneUpdateForm personne) {
 
+        Long returnId = personneService.update(id, personne.toEntity());
+        return ResponseEntity.ok(returnId);
+
+    }
+
+    @PreAuthorize("hasAuthority('CITOYEN')")
+    @PutMapping
+    public ResponseEntity<Long> updatePersonne(@RequestBody @Valid PersonneUpdateForm personne, Authentication auth) {
+
+        Utilisateur c = (Utilisateur) auth.getPrincipal();
+        Long id = c.getPersonne().getId();
         Long returnId = personneService.update(id, personne.toEntity());
         return ResponseEntity.ok(returnId);
 
