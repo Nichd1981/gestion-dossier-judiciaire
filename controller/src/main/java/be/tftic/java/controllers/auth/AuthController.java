0432d0b6cd0f604b929f3e08be2ecda560734afd
Azzedine Hassaini <azzedinehassaini@gmail.com>
2024-07-04 13:43:31 +0200

    Refacto

--- controller/src/main/java/be/tftic/java/controllers/auth/AuthController.java
+++ controller/src/main/java/be/tftic/java/controllers/auth/AuthController.java
@@ -4,22 +4,18 @@ import be.tftic.java.bll.services.UserService;
 import be.tftic.java.common.models.requests.auth.LoginRequest;
 import be.tftic.java.common.models.requests.auth.RegisterRequest;
 import be.tftic.java.common.models.responses.UserTokenResponse;
-import be.tftic.java.domain.entities.User;
 import be.tftic.java.domain.enums.Role;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * Contrôleur REST pour la gestion de l'authentification des utilisateurs.
- *
- * @RestController indique que cette classe est un Contrôleur REST.
- * @RequiredArgsConstructor indique que le compilateur doit générer un constructeur avec les arguments requis.
- * @RequestMapping("/auth") indique que les requêtes HTTP qui correspondent à cette annotation seront traitées par cette classe.
+ * RestController indique que cette classe est un Contrôleur REST.
+ * RequiredArgsConstructor indique que le compilateur doit générer un constructeur avec les arguments requis.
+ * RequestMapping("/auth") indique que les requêtes HTTP qui correspondent à cette annotation seront traitées par cette classe.
  */
 @RestController
 @RequiredArgsConstructor
@@ -34,9 +30,8 @@ public class AuthController {
 
     /**
      * Traite une requête HTTP POST pour l'authentification d'un utilisateur.
-     *
-     * @PreAuthorize("isAnonymous()") indique que cette méthode ne peut être appelée que si l'utilisateur n'est pas authentifié.
-     * @PostMapping("/login") indique que cette méthode traite les requêtes HTTP POST envoyées à l'URL "/login".
+     * PreAuthorize("isAnonymous()") indique que cette méthode ne peut être appelée que si l'utilisateur n'est pas authentifié.
+     * PostMapping("/login") indique que cette méthode traite les requêtes HTTP POST envoyées à l'URL "/login".
      *
      * @param form les informations de connexion de l'utilisateur.
      * @return une réponse HTTP avec les informations de l'utilisateur et un jeton JWT.
