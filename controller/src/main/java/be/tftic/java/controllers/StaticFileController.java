160f24614c94a1365d2c978e6f34928e3cb1444d
Azzedine Hassaini <azzedinehassaini@gmail.com>
2024-07-10 13:27:21 +0200

    StaticFileCOntroller : Upload & get image

--- controller/src/main/java/be/tftic/java/controllers/StaticFileController.java
+++ controller/src/main/java/be/tftic/java/controllers/StaticFileController.java
@@ -6,20 +6,37 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Controller
 public class StaticFileController {
 
-    private static final String FILE_DIRECTORY = "C:\\Users\\a.georis\\Desktop\\Spring\\gestion-dossier-judiciaire\\uploads";
+    @PostMapping("/image")
+    public ResponseEntity<Void> uploadFile(@RequestParam("file") MultipartFile file) {
+        // The "public" directory is automatically used by Spring to serve static assets
+        Path publicDirectory = Paths.get(".", "public").toAbsolutePath();
+        Path filepath = Paths.get(publicDirectory.toString(), file.getOriginalFilename());
+        try (OutputStream os = Files.newOutputStream(filepath)) {
+            byte[] imageContent = file.getBytes();
+            os.write(imageContent);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
+    }
 
     @GetMapping("/image/{fileName:.+}")
-    public ResponseEntity<FileSystemResource> serveFile(@PathVariable String fileName) {
+    public ResponseEntity<FileSystemResource> getFile(@PathVariable String fileName) {
+        Path publicDirectory = Paths.get(".", "public").toAbsolutePath();
         try {
-            Path path = Paths.get(FILE_DIRECTORY, fileName);
+            Path path = Paths.get(publicDirectory.toString(), fileName);
             FileSystemResource resource = new FileSystemResource(path);
 
             if (resource.exists()) {
