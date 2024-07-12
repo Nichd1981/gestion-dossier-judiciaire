4d4eeee0a7353d28603adfa5b749c119653db560
Antoine Georis <antoinegeoris@outlook.be>
2024-07-12 11:09:49 +0200

    update et modif back-end

--- controller/src/main/java/be/tftic/java/controllers/StaticFileController.java
+++ controller/src/main/java/be/tftic/java/controllers/StaticFileController.java
@@ -4,10 +4,7 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.OutputStream;
@@ -16,6 +13,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Controller
+@CrossOrigin("*")
 public class StaticFileController {
 
     @PostMapping("/image")
