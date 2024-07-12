82676bc01bbb5d687b3765181b8c759383e00a6b
Azzedine Hassaini <azzedinehassaini@gmail.com>
2024-07-12 10:30:07 +0200

    DataInitializer : new set of test datas

--- config/src/main/java/be/tftic/java/il/utils/DataInitializer.java
+++ config/src/main/java/be/tftic/java/il/utils/DataInitializer.java
@@ -32,8 +32,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
-    //TODO : pdt le refactoring, passer par les service plutot que les repository
-
     private final PersonRepository personRepository;
     private final AddressRepository addressRepository;
     private final PhoneRepository phoneRepository;
@@ -52,6 +50,104 @@ public class DataInitializer implements CommandLineRunner {
     @Override
     public void run(String... args) {
         if (personRepository.count()==0){
+//                Person azzedine = new Person("881113-237-37","Hassaini", "Azzedine", LocalDate.of(1988,11,13), "La Louvière", Gender.MALE, null, "", "");
+//                Person antoine = new Person("881113-237-36","Georis", "Antoine", LocalDate.of(1999,9,9), "Charleroi", Gender.MALE, null, "", "");
+//                Person nicolas = new Person("810208-183-31","Quinet", "Nicolas", LocalDate.of(1981,2,8), "Ottignies", Gender.MALE, null, "mario.jpg", "");
+//                Person valentine = new Person("112233-123-34","Collignon", "Valentine", LocalDate.of(1999,3,23), "Dinant", Gender.FEMALE, null, "", "");
+//                valentine.setLawyer(azzedine);
+//                personRepository.save(azzedine);
+//                personRepository.save(valentine);
+//                personRepository.save(antoine);
+//                personRepository.save(nicolas);
+//
+//                User admin = User.builder()
+//                        .mail("antoinegeoris@outlook.be")
+//                        .password(passwordEncoder.encode("12341234"))
+//                        .role(Role.ADMIN)
+//                        .person(antoine)
+//                        .build();
+//
+//                User agent = User.builder()
+//                        .mail("quinet.nicolas@gmail.com")
+//                        .password(passwordEncoder.encode("12341234"))
+//                        .role(Role.AGENT)
+//                        .person(nicolas)
+//                        .build();
+//
+//                User citizen = User.builder()
+//                        .mail("a.hassaini@stag.technofuturtic.education")
+//                        .password(passwordEncoder.encode("12341234"))
+//                        .role(Role.CITIZEN)
+//                        .person(azzedine)
+//                        .build();
+//
+//                User citizen2 = User.builder()
+//                        .mail("antoinegeoris99@outlook.be")
+//                        .password(passwordEncoder.encode("12341234"))
+//                        .role(Role.CITIZEN)
+//                        .person(antoine)
+//                        .build();
+//
+//                User lawyer = User.builder()
+//                        .mail("valentine@gmail.com")
+//                        .password(passwordEncoder.encode("12341234"))
+//                        .role(Role.LAWYER)
+//                        .person(valentine)
+//                        .build();
+//
+//                userRepository.save(admin);
+//                userRepository.save(agent);
+//                userRepository.save(citizen);
+//                userRepository.save(citizen2);
+//                userRepository.save(lawyer);
+//
+//                Address address1 = new Address("Rue test", "1", "Ville", "1234", "Belgique", "Domicile", azzedine);
+//                Address address2 = new Address("Rue test2", "1", "Ville", "1234", "Belgique", "Domicile", nicolas);
+//                Address address3 = new Address("Rue Joseph Van Brusselt", "2C", "Charleroi", "6280", "Belgique", "Domicile", antoine);
+//                Address address4 = new Address("Rue de la paix", "50", "Charleroi", "6044", "Belgique", "Domicile", antoine);
+//                addressRepository.save(address1);
+//                addressRepository.save(address2);
+//                addressRepository.save(address3);
+//                addressRepository.save(address4);
+//
+//                Phone phone1 = new Phone("0498123456", "GSM", azzedine);
+//                Phone phone2 = new Phone("0498123457", "GSM", valentine);
+//                Phone phone3 = new Phone("0493760903", "GSM", antoine);
+//                Phone phone4 = new Phone("0498123459", "GSM", nicolas);
+//                phoneRepository.save(phone1);
+//                phoneRepository.save(phone2);
+//                phoneRepository.save(phone3);
+//                phoneRepository.save(phone4);
+//
+//                Complaint complaint = new Complaint("VAL-1234-5678", ComplaintStatus.REGISTERED, LocalDate.of(2024,1,10).atStartOfDay(), valentine, nicolas);
+//                complaint.getPersonConcerned().add(antoine);
+//                complaintRepository.save(complaint);
+//
+//                Complaint complaint2 = new Complaint("AZZ-1234-5679", ComplaintStatus.REGISTERED, LocalDate.of(2024,2,10).atStartOfDay(), azzedine, nicolas);
+//                complaint2.getPersonConcerned().add(antoine);
+//                complaintRepository.save(complaint2);
+//
+//                Complaint complaint3 = new Complaint("VAL-1234-5670", ComplaintStatus.IN_PROGRESS, LocalDate.of(2024,3,10).atStartOfDay(), valentine, nicolas);
+//                complaint3.getPersonConcerned().add(antoine);
+//                complaintRepository.save(complaint3);
+//
+//                Complaint complaint4 = new Complaint("ANT-1234-5670", ComplaintStatus.IN_PROGRESS, LocalDate.of(2024,4,10).atStartOfDay(), antoine, nicolas);
+//                complaint4.getPersonConcerned().add(valentine);
+//                complaintRepository.save(complaint4);
+//
+//                Deposition deposition = new Deposition(LocalDate.of(2024, 1, 5), "Ceci est une arnaque !", complaint);
+//                depositionRepository.save(deposition);
+//                Deposition deposition2 = new Deposition(LocalDate.of(2024, 10, 5), "C'est quoi ça encore ?!", complaint);
+//                depositionRepository.save(deposition2);
+//                Deposition deposition3 = new Deposition(LocalDate.of(2024, 5, 5), "Je suis perdu !", complaint);
+//                depositionRepository.save(deposition3);
+//
+//                Audition audition  = new Audition(LocalDateTime.of(2024, 1, 5, 10, 0), "2", "Ceci est une audition !", azzedine, nicolas, valentine, complaint );
+//                auditionRepository.save(audition);
+//                Audition audition2  = new Audition(LocalDateTime.of(2025, 7, 7, 10, 0), "2", "Ceci est une audition Bis repetita !", azzedine, nicolas, valentine, complaint );
+//                auditionRepository.save(audition2);
+
+
 
             // Personnes
             List<Person> persons = new ArrayList<>();
@@ -65,9 +161,16 @@ public class DataInitializer implements CommandLineRunner {
             persons.add(new Person("940808-890-08", "Petit", "Anne", LocalDate.of(1994, 8, 8), "Bruges", Gender.FEMALE, null, "", ""));
             persons.add(new Person("890909-901-09", "Moreau", "Thomas", LocalDate.of(1989, 9, 9), "Louvain", Gender.MALE, null, "", ""));
             persons.add(new Person("960101-012-10", "Legrand", "Isabelle", LocalDate.of(1996, 1, 1), "Verviers", Gender.FEMALE, null, "", ""));
-            persons.forEach(person -> {
-                person.setLawyer(persons.get(3));
-            });
+            for (int i = 0; i < persons.size(); i++) {
+                if (i == 3)
+                    persons.get(i).setLawyer(persons.get(7));
+                else if (i == 7)
+                    persons.get(i).setLawyer(persons.get(3));
+                else if (i %2 == 0)
+                    persons.get(i).setLawyer(persons.get(7));
+                else
+                    persons.get(i).setLawyer(persons.get(3));
+            }
             personRepository.saveAll(persons);
 
             // Adresses et numéros de téléphone
@@ -113,25 +216,25 @@ public class DataInitializer implements CommandLineRunner {
             // Plaintes (20)
             List<Complaint> complaints = new ArrayList<>();
             complaints.add(new Complaint("DUP-2024-0001", ComplaintStatus.REGISTERED, LocalDate.of(2024, 1, 15).atStartOfDay(), persons.get(0), persons.get(1)));
-            complaints.add(new Complaint("MAR-2024-0002", ComplaintStatus.IN_PROGRESS, LocalDate.of(2024, 2, 20).atStartOfDay(), persons.get(1), persons.get(2)));
-            complaints.add(new Complaint("LER-2024-0003", ComplaintStatus.CLOSED, LocalDate.of(2024, 3, 10).atStartOfDay(), persons.get(2), persons.get(3)));
-            complaints.add(new Complaint("DUB-2024-0004", ComplaintStatus.REGISTERED, LocalDate.of(2024, 4, 5).atStartOfDay(), persons.get(3), persons.get(4)));
+            complaints.add(new Complaint("MAR-2024-0002", ComplaintStatus.IN_PROGRESS, LocalDate.of(2024, 2, 20).atStartOfDay(), persons.get(1), persons.get(5)));
+            complaints.add(new Complaint("LER-2024-0003", ComplaintStatus.CLOSED, LocalDate.of(2024, 3, 10).atStartOfDay(), persons.get(2), persons.get(9)));
+            complaints.add(new Complaint("DUB-2024-0004", ComplaintStatus.REGISTERED, LocalDate.of(2024, 4, 5).atStartOfDay(), persons.get(3), persons.get(1)));
             complaints.add(new Complaint("LAM-2024-0005", ComplaintStatus.IN_PROGRESS, LocalDate.of(2024, 5, 12).atStartOfDay(), persons.get(4), persons.get(5)));
-            complaints.add(new Complaint("ROU-2024-0006", ComplaintStatus.CLOSED, LocalDate.of(2024, 6, 18).atStartOfDay(), persons.get(5), persons.get(6)));
-            complaints.add(new Complaint("LEF-2024-0007", ComplaintStatus.REGISTERED, LocalDate.of(2024, 7, 22).atStartOfDay(), persons.get(6), persons.get(7)));
-            complaints.add(new Complaint("PET-2024-0008", ComplaintStatus.IN_PROGRESS, LocalDate.of(2024, 8, 30).atStartOfDay(), persons.get(7), persons.get(8)));
+            complaints.add(new Complaint("ROU-2024-0006", ComplaintStatus.CLOSED, LocalDate.of(2024, 6, 18).atStartOfDay(), persons.get(5), persons.get(9)));
+            complaints.add(new Complaint("LEF-2024-0007", ComplaintStatus.REGISTERED, LocalDate.of(2024, 7, 22).atStartOfDay(), persons.get(6), persons.get(1)));
+            complaints.add(new Complaint("PET-2024-0008", ComplaintStatus.IN_PROGRESS, LocalDate.of(2024, 8, 30).atStartOfDay(), persons.get(7), persons.get(5)));
             complaints.add(new Complaint("MOR-2024-0009", ComplaintStatus.CLOSED, LocalDate.of(2024, 9, 7).atStartOfDay(), persons.get(8), persons.get(9)));
-            complaints.add(new Complaint("LEG-2024-0010", ComplaintStatus.REGISTERED, LocalDate.of(2024, 10, 14).atStartOfDay(), persons.get(9), persons.get(0)));
-            complaints.add(new Complaint("DUP-2024-0011", ComplaintStatus.IN_PROGRESS, LocalDate.of(2024, 11, 19).atStartOfDay(), persons.get(0), persons.get(2)));
-            complaints.add(new Complaint("MAR-2024-0012", ComplaintStatus.CLOSED, LocalDate.of(2024, 12, 25).atStartOfDay(), persons.get(1), persons.get(3)));
-            complaints.add(new Complaint("LER-2025-0001", ComplaintStatus.REGISTERED, LocalDate.of(2025, 1, 2).atStartOfDay(), persons.get(2), persons.get(4)));
+            complaints.add(new Complaint("LEG-2024-0010", ComplaintStatus.REGISTERED, LocalDate.of(2024, 10, 14).atStartOfDay(), persons.get(9), persons.get(1)));
+            complaints.add(new Complaint("DUP-2024-0011", ComplaintStatus.IN_PROGRESS, LocalDate.of(2024, 11, 19).atStartOfDay(), persons.get(0), persons.get(5)));
+            complaints.add(new Complaint("MAR-2024-0012", ComplaintStatus.CLOSED, LocalDate.of(2024, 12, 25).atStartOfDay(), persons.get(1), persons.get(9)));
+            complaints.add(new Complaint("LER-2025-0001", ComplaintStatus.REGISTERED, LocalDate.of(2025, 1, 2).atStartOfDay(), persons.get(2), persons.get(1)));
             complaints.add(new Complaint("DUB-2025-0002", ComplaintStatus.IN_PROGRESS, LocalDate.of(2025, 2, 8).atStartOfDay(), persons.get(3), persons.get(5)));
-            complaints.add(new Complaint("LAM-2025-0003", ComplaintStatus.CLOSED, LocalDate.of(2025, 3, 15).atStartOfDay(), persons.get(4), persons.get(6)));
-            complaints.add(new Complaint("ROU-2025-0004", ComplaintStatus.REGISTERED, LocalDate.of(2025, 4, 21).atStartOfDay(), persons.get(5), persons.get(7)));
-            complaints.add(new Complaint("LEF-2025-0005", ComplaintStatus.IN_PROGRESS, LocalDate.of(2025, 5, 27).atStartOfDay(), persons.get(6), persons.get(8)));
+            complaints.add(new Complaint("LAM-2025-0003", ComplaintStatus.CLOSED, LocalDate.of(2025, 3, 15).atStartOfDay(), persons.get(4), persons.get(9)));
+            complaints.add(new Complaint("ROU-2025-0004", ComplaintStatus.REGISTERED, LocalDate.of(2025, 4, 21).atStartOfDay(), persons.get(5), persons.get(1)));
+            complaints.add(new Complaint("LEF-2025-0005", ComplaintStatus.IN_PROGRESS, LocalDate.of(2025, 5, 27).atStartOfDay(), persons.get(6), persons.get(5)));
             complaints.add(new Complaint("PET-2025-0006", ComplaintStatus.CLOSED, LocalDate.of(2025, 6, 3).atStartOfDay(), persons.get(7), persons.get(9)));
-            complaints.add(new Complaint("MOR-2025-0007", ComplaintStatus.REGISTERED, LocalDate.of(2025, 7, 9).atStartOfDay(), persons.get(8), persons.get(0)));
-            complaints.add(new Complaint("LEG-2025-0008", ComplaintStatus.IN_PROGRESS, LocalDate.of(2025, 8, 16).atStartOfDay(), persons.get(9), persons.get(1)));
+            complaints.add(new Complaint("MOR-2025-0007", ComplaintStatus.REGISTERED, LocalDate.of(2025, 7, 9).atStartOfDay(), persons.get(8), persons.get(1)));
+            complaints.add(new Complaint("LEG-2025-0008", ComplaintStatus.IN_PROGRESS, LocalDate.of(2025, 8, 16).atStartOfDay(), persons.get(9), persons.get(5)));
             complaintRepository.saveAll(complaints);
 
             // Dépositions
